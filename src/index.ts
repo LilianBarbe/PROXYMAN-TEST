80329f0612022f69176cd0ebfb9ba81dcb8e44c5
LilianBarbe <110779804+LilianBarbe@users.noreply.github.com>
2024-03-01 09:00:05 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { gsap } from 'gsap';
+
 import { greetUser } from '$utils/greet';
+
+gsap.from('.fs0', { opacity: 0, duration: 3, y: 100 });
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
